2d00edc16772295efe4af645d703284caf9f9e13
SamMayer0815 <sammayer0815@gmail.com>
2022-05-24 14:14:20 +0200

    target go up and down smooth
    if enter zone it goes up and then if hit it goes down smooth and can only be shot if its standing up

--- Gun Down The Targets/Assets/scripts/targetHit.cs
+++ Gun Down The Targets/Assets/scripts/targetHit.cs
@@ -5,36 +5,61 @@ using UnityEngine;
 public class targetHit : MonoBehaviour
 {
     public bool isHit = false;
-    public bool canBeHit = true;
-    public bool rotated = false;
-    public float Rotspeed = 2f;
+    public bool canBeHit = false;
+    public bool rotatedDown = false;
+    public bool rotatedUp = false;
+    public bool enteredZone = false;
+    public float rotSpeedUp = -2f;
+    public float rotSpeedDown = 2f;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        transform.rotation = Quaternion.Euler(0, 0, 90);
     }
 
     // Update is called once per frame
     void Update()
     {
+        if(isHit && !canBeHit)
+        {
+            isHit = false;
+        }
+
         if (isHit && canBeHit)
         {
             canBeHit = false;
-            rotated = true;
-            Debug.Log("werkt");
+            rotatedDown = true;
+            rotatedUp = false;
         }
 
-        if (rotated)
+        if (enteredZone)
         {
-            transform.Rotate(0, 0, Rotspeed);
-            Debug.Log(transform.rotation.z);
+            rotatedUp = true;
+            enteredZone = false;
         }
-        if (transform.rotation.z > 0.7)
+
+        if (rotatedUp)
         {
-            transform.rotation = Quaternion.Euler(0, 0, 90); 
-            rotated = false;
-            Debug.Log("werkt90");
+            transform.Rotate(0, 0, rotSpeedUp);
+        }
+
+        if (transform.rotation.z < 0 && rotatedUp)
+        {
+            transform.rotation = Quaternion.Euler(0, 0, 0);
+            rotatedUp = false;
+            canBeHit = true;
+        }
+
+        if (rotatedDown)
+        {
+            transform.Rotate(0, 0, rotSpeedDown);
+        }
+
+        if (transform.rotation.z > 0.7 && rotatedDown)
+        {
+            transform.rotation = Quaternion.Euler(0, 0, 90);
+            rotatedDown = false;
         }
     }
 }
