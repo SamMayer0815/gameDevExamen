10e1ab523d545b48479c1cb2dbce6fbfb4836b9b
SamMayer0815 <sammayer0815@gmail.com>
2022-05-25 09:26:40 +0200

    fixed shoot thru zone
    fixed it so that if you enter zone it disables the zone collider so that if i leave the zone i can still hit the targets instead of hitting the zone collider

--- Gun Down The Targets/Assets/scripts/zoneManager.cs
+++ Gun Down The Targets/Assets/scripts/zoneManager.cs
@@ -5,12 +5,18 @@ using UnityEngine;
 public class zoneManager : MonoBehaviour
 {
     private bool hasBeenEntered = false;
+    Collider collider;
+    private void Start()
+    {
+        collider = GetComponent<Collider>();
+    }
     private void OnTriggerEnter(Collider col)
     {
         //checks if the player entered the zone and if he hasent entered it before
         if (col.gameObject.tag == "Player" && !hasBeenEntered)
         {
             hasBeenEntered = true;
+            collider.enabled = !collider.enabled;
             //sets childs to ammount of targets in zone
             int childs = transform.childCount;
             //for each target in the area it sets that the player has entered the zone so that the target goes up
@@ -21,3 +27,4 @@ public class zoneManager : MonoBehaviour
         }
     }
 }
+    
