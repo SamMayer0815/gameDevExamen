13615b8c5e546e3755b7af36cc6a4e6e4c33913d
SamMayer0815 <sammayer0815@gmail.com>
2022-05-30 12:46:21 +0200

    aim and reload script
    aim and reload script

--- Gun Down The Targets/Assets/scripts/aimScript.cs
+++ Gun Down The Targets/Assets/scripts/aimScript.cs
@@ -4,18 +4,36 @@ using UnityEngine;
 
 public class aimScript : MonoBehaviour
 {
-    [SerializeField] private Animator aiming;
+    private Animator reloading;
+    public GameObject aimPosition;
+    private void Start()
+    {
+        reloading = GameObject.Find("reload").GetComponent<Animator>();
+    }
 
     // Update is called once per frame
     void Update()
     {
         if (Input.GetButton("Fire2"))
         {
-            aiming.SetBool("isAiming", true);
-        }else
-        {
-            aiming.SetBool("isAiming", false);
+            aimPosition.transform.localPosition = new Vector3(-0.262f, 0.162f, 0);
         }
+        else if(!Input.GetButton("Fire2"))
+        {
+            aimPosition.transform.localPosition = new Vector3(0, 0, 0);
+            Debug.Log("t");
+        }
+
+        if (Input.GetKeyDown("r"))
+        {
+            reloading.Play("Base Layer.reload",0 ,0);
+            aimPosition.transform.localPosition = new Vector3(0, 0, 0);
+            reloadWait();
+        }
+    }
+    IEnumerator reloadWait()
+    {
+        yield return new WaitForSeconds(3);
     }
 }
 //(0.33, -0.28, 0.56)
