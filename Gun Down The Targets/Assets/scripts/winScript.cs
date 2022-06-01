e4aeb80c4655afb7e2d753681ac2956304b7ef6e
SamMayer0815 <sammayer0815@gmail.com>
2022-06-01 08:23:22 +0200

    main menu complete
    completed main menu

--- Gun Down The Targets/Assets/scripts/winScript.cs
+++ Gun Down The Targets/Assets/scripts/winScript.cs
@@ -1,19 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class winScript : MonoBehaviour
 {
     private GameObject gameObjects;
     private int enemyCount;
     private shootScript playerScript;
+    private Scene scene;
+    private string currentScene;
     // Start is called before the first frame update
     void Start()
     {
         GameObject[] gameObjects;
         gameObjects = GameObject.FindGameObjectsWithTag("Enemy");
-
         enemyCount = gameObjects.Length;
+        scene = SceneManager.GetActiveScene();
+        currentScene = scene.name;
     }
 
     // Update is called once per frame
@@ -25,5 +29,11 @@ public class winScript : MonoBehaviour
         {
             Debug.Log("Win");
         }
+
+    }
+
+    void nextLevel()
+    {
+
     }
 }
